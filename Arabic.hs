ccef47a31d05e2eb303403a6f5ca923d5928df6d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2016-11-10 14:44:54 +0100

    improve program ui

--- Arabic.hs
+++ Arabic.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
-{-# OPTIONS_GHC -fdefer-type-errors #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 import Text.HTML.Scalpel
 import Data.Default (def)
 import Data.Encoding
 import qualified Network.Curl as Curl
 import Data.Encoding.CP1256
+import System.Environment
+import Control.Monad
 
 type Author = String
 
@@ -19,8 +20,8 @@ specificDecoder = decodeStrictByteString CP1256 . Curl.respBody
 cfg :: Config String
 cfg = (def :: Config String) {decoder = specificDecoder :: Decoder String}
 
-allComments :: IO (Maybe [Comment])
-allComments = scrapeURLWithConfig cfg "http://www.4algeria.com/vb/4algeria456289/" posts
+allComments :: String -> IO (Maybe [Comment])
+allComments url = scrapeURLWithConfig cfg url posts
    where
      posts :: Scraper String [Comment]
      posts = chroots ("div" @: [hasClass "PostbitContainer"]) $
@@ -28,5 +29,16 @@ allComments = scrapeURLWithConfig cfg "http://www.4algeria.com/vb/4algeria456289
 
 main :: IO ()
 main = do
-  Just xs <- allComments
-  Prelude.writeFile "results" $ Prelude.unlines [c | TextComment c <- xs]
+  args <- getArgs
+  (xs,url) <- case args of
+    [url] -> do
+      Just xs <- allComments url
+      return (xs,url)
+    [url,n] -> do
+      xss <- forM [1..read n] $ \i -> do
+        let url' = if i == 1 then url else url ++ "-" ++ show i
+        Just xs <- allComments url'
+        return xs
+      return (concat xss,url)
+
+  Prelude.writeFile ("results-" ++ (filter (/= '/') url)) $ Prelude.unlines [c | TextComment c <- xs]
